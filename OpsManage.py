1b9d97d0bac81cfa54f6bec4c16f5c553b7fe05e
luoshenguidao <fallenleaves@live.cn>
2017-12-05 15:35:36 +0800

    delete db

--- OpsManage.py
+++ OpsManage.py
@@ -2,31 +2,18 @@
 import sqlite3
 import os
 
-ip = str(input("plese input something:"))
-conn = sqlite3.connect('luoshen.db')
-cr = conn.cursor()
-
-
-def createdata():
-    cr.execute('create table user (id varchar(20) primary key, name varchar(20))')
-
-
-def insertdata():
-    cr.execute("insert into user (id, name) values (10,'10.125.48.21');")
-    conn.commit()
-    conn.close()
-    # cr.close()
-    print("has been close")
+message = str(input("plese input something:"))
+connect = sqlite3.connect('applist.db')
+cursor = connect.cursor()
 
 
 def selectdata():
-    values = cr.execute('select * from user where name = ' + "'" + ip +"'")
+    values = cursor.execute('select * from user where appname = ' + "'" + message + "'")
     for row in values:
         print("id ", row[0])
         print("name", row[1])
     print("信息已查出.")
     conn.close()
+    connect.close()
 
-
-# insertdata()
 selectdata()
