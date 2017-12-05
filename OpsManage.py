2d71955bb22735c0976da3949435fe9c4510d983
luoshenguidao <fallenleaves@live.cn>
2017-12-05 17:08:54 +0800

    增加输出格式为json

--- OpsManage.py
+++ OpsManage.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import sqlite3
-import os
+import json
 
 message = str(input("plese input something:"))
 connect = sqlite3.connect('applist.db')
@@ -8,14 +8,25 @@ cursor = connect.cursor()
 
 
 def selectdata():
-    values = cursor.execute('select * from app where appname = ' + "'" + message + "'")
-    for row in values:
-        print("id ", row[0])
-        print("department", row[1])
-        print("appname", row[2])
+    cursor.execute('select * from app where appname = ' + "'" + message + "'")
+    value = cursor.fetchall()
+    jsonDataraw = []
+    for row in value:
+        result = {}
+        result['id'] = row[0]
+        result['department'] = row[1]
+        result['appname'] = row[2]
+        result['ip'] = row[3]
+        result['instance'] = row[4]
+        result['port'] = row[5]
+        result['memery'] = row[6]
+        result['type'] = row[7]
+        jsonDataraw.append(result)
+    jsondata = json.dumps(jsonDataraw, ensure_ascii=False)
+    print(jsondata[1:len(jsondata) - 1])
+
     print("信息已查出.")
     cursor.close()
     connect.close()
-
 
 selectdata()
