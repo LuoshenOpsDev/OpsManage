3c77ee71f222cc93c024cd06fe46ff0fe1a0e07e
luoshenguidao <fallenleaves@live.cn>
2017-12-07 10:53:34 +0800

    更新字典方式

--- OpsManage.py
+++ OpsManage.py
@@ -10,23 +10,17 @@ cursor = connect.cursor()
 def selectdata():
     cursor.execute('select * from app where appname = ' + "'" + message + "'")
     value = cursor.fetchall()
-    jsonDataraw = []
+    jsondataraw = []
     for row in value:
-        result = {}
-        result['id'] = row[0]
-        result['department'] = row[1]
-        result['appname'] = row[2]
-        result['ip'] = row[3]
-        result['instance'] = row[4]
-        result['port'] = row[5]
-        result['memery'] = row[6]
-        result['type'] = row[7]
-        jsonDataraw.append(result)
-    jsondata = json.dumps(jsonDataraw, ensure_ascii=False)
+        result = {'id': row[0], 'department': row[1], 'appname': row[2], 'ip': row[3],
+                  'instance': row[4], 'port': 'row[5]', 'memery': row[6], 'type': row[7]}
+        jsondataraw.append(result)
+    jsondata = json.dumps(jsondataraw, ensure_ascii=False)
     print(jsondata[1:len(jsondata) - 1])
 
     print("信息已查出.")
     cursor.close()
     connect.close()
+
 
 selectdata()
